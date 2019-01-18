770f06bbb401c53f19ad26015c990d91a15f84c1
sralloza <sralloza@gmail.com>
2019-01-18 10:30:08 +0100

    Pylinted keys manager

--- managers/keys_manager.py
+++ managers/keys_manager.py
@@ -1,61 +1,88 @@
 # -*- coding: utf-8 -*-
 
+"""Password manager for raspberry pi scripts."""
+
 import json
+import logging
+from typing import Tuple
 
 from rpi.dns import RpiDns
 from rpi.exceptions import MissingKeyError
-from rpi.rpi_logging import Logging
 
 
-class KeysManager(object):
-    # TODO: improve: I can use some kind of token, and store them in a sqlite database or at least encrypt the json.
+class KeysManager:
+    # TODO: improve: I can use some kind of token, and store them in a sqlite database or at
+    #  least encrypt the json.
     """Manages the passwords."""
 
     def __init__(self):
-        self.logger = Logging.get(__file__, __name__)
+        self.logger = logging.getLogger(__name__)
         self.path = RpiDns.get('json.claves')
-        with open(self.path) as f:
-            self.dict = json.load(f)
+        with open(self.path) as file_handler:
+            self.dict = json.load(file_handler)
 
     @staticmethod
-    def keys():
+    def keys() -> Tuple[str]:
+        """Returns all the keys for the passwords.
+
+        Returns:
+            Tuple[str]: keys of the passwords
+
+        """
         self = object.__new__(KeysManager)
         self.__init__()
 
         result = tuple(self.dict.keys())
 
-        self.logger.debug(f'Returning list of keys - {result!r}', enable=False)
+        self.logger.debug('Returning list of keys - %r', result)
 
         return result
 
     def save(self):
+        """Saves the passwords."""
         if self.dict is not None:
-            with open(self.path, 'wt') as f:
-                json.dump(self.dict, f, ensure_ascii=False, indent=4, sort_keys=True)
+            with open(self.path, 'wt') as file_handler:
+                json.dump(self.dict, file_handler, ensure_ascii=False, indent=4, sort_keys=True)
 
-        self.logger.debug(f'Saved keys successfully')
+        self.logger.debug('Saved keys successfully')
 
     @staticmethod
-    def set(service, password):
+    def set(service: str, password: str):
+        """Sets a password  given its password and service.
+
+        Args:
+            service (str): service of the password.
+            password (str): password to store.
+
+        """
         self = object.__new__(KeysManager)
         self.__init__()
 
-        self.logger.debug(f'Saving password for key {service!r}')
+        self.logger.debug('Saving password for key %r', service)
 
         self.dict[service] = password
         self.save()
 
     @staticmethod
-    def get(key):
+    def get(key: str) -> str:
+        """Returns the password of the key.
+
+        Args:
+            key (str): service of the password to get.
+
+        Returns:
+            str: password.
+
+        """
         self = object.__new__(KeysManager)
         self.__init__()
 
-        self.logger.debug(f'Searching key for {key!r}', enable=False)
+        self.logger.debug('Searching key for %r', key)
 
         if key not in self.keys():
-            self.logger.critical(f'Key {key!r} not found')
+            self.logger.critical('Key %r not found', key)
             raise MissingKeyError(f'Key {key!r} not found')
 
-        self.logger.debug(f'Password found for key {key!r}')
+        self.logger.debug('Password found for key %r', key)
 
         return self.dict[key]
