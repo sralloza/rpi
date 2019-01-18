b92289f7fbbb04062dfd6b7e50334811cd4d9b8e
sralloza <sralloza@gmail.com>
2019-01-18 10:29:49 +0100

    Pylinted config manager

--- managers/config_manager.py
+++ managers/config_manager.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
+"""Configuration manager for raspberry pi.
+Designed to easilly use and store pairs of key - value.
+"""
+import logging
+
 from rpi.dns import RpiDns
 from rpi.exceptions import ConfigNotFoundError, EmptyConfigError, ConfigError
-from rpi.rpi_logging import Logging
 
 
-class ConfigManager(object):
+class ConfigManager:
+    """Configuration manager."""
+
     def __init__(self):
-        self.logger = Logging.get(__file__, __name__)
+        self.logger = logging.getLogger(__name__)
         self.path = RpiDns.get('textdb.config')
         self.config = {}
         self.load()
@@ -31,12 +37,14 @@ class ConfigManager(object):
         return item in self.config
 
     def load(self):
-        # self.logger.debug('Loading config')
+        """Loads configurations from the main file."""
+
+        self.logger.debug('Loading config')
         try:
-            with open(self.path, 'rt', encoding='utf-8') as fh:
-                data = fh.read()
+            with open(self.path, 'rt', encoding='utf-8') as file_handler:
+                data = file_handler.read()
         except FileNotFoundError:
-            self.logger.critical(f'Config file does not exist ({self.path!r})')
+            self.logger.critical('Config file does not exist (%r)', self.path)
             raise FileNotFoundError(f'Config file does not exist ({self.path!r})')
 
         for line in data.splitlines():
@@ -47,32 +55,54 @@ class ConfigManager(object):
 
             self[key] = value
 
-    def save(self, force=False):
+    def save(self, force: bool = False):
+        """Saves all the configurations in the file.
+
+        Args:
+            force (bool): tells the function to save configurations even if there are no
+                configurations to save.
+
+        Raises:
+             EmptyConfigError: if there are no configurations to save and force is False.
+             ConfigError: if the user is trying to save less configurations than the ones that
+                are currently saved.
+
+        """
 
         other = ConfigManager.__new__(ConfigManager)
         other.__init__()
 
         if len(other) > len(self):
-            self.logger.critical('You can not save less configurations than the ones that are saved')
+            self.logger.critical(
+                'You can not save less configurations than the ones that are saved')
             raise ConfigError('You can not save less configurations than the ones that are saved')
 
         if len(self) == 0 and force is False:
             self.logger.critical('There are no configurations to save')
             raise EmptyConfigError('There are no configurations to save')
 
-        with open(self.path, 'wt', encoding='utf-8') as fh:
+        with open(self.path, 'wt', encoding='utf-8') as file_handler:
             for key, value in self.config.items():
-                fh.write(f"{key}={value}\n")
+                file_handler.write(f"{key}={value}\n")
 
-        self.logger.debug(f'Configurations saved (force={force})')
+        self.logger.debug('Configurations saved (force=%s)', force)
 
     @staticmethod
-    def get(config):
+    def get(config: str) -> str:
+        """Gets a configuration value by its key.
+
+        Args:
+            config (str): key for the configuration.
+
+        Returns:
+            str:  value for the configuration.
+
+        """
         self = ConfigManager.__new__(ConfigManager)
         self.__init__()
 
         if config not in self:
-            self.logger.critical(f'Configuration not found: {config!r}')
+            self.logger.critical('Configuration not found: %r', config)
             raise ConfigNotFoundError(f'Configuration not found: {config!r}')
 
         valor = self[config]
@@ -82,34 +112,54 @@ class ConfigManager(object):
         return valor
 
     @staticmethod
-    def set(config, value):
+    def set(config: str, value: str):
+        """Sets a configuration value using its key.
+
+        Args:
+            config (str): key of the configuration.
+            value (str): value of the configuration.
+
+        """
+
         self = ConfigManager.__new__(ConfigManager)
         self.__init__()
 
         self[config] = value
 
-        self.logger.debug(f'Set config value {value!r} with key {config!r}')
+        self.logger.debug('Set config value %r with key %r', value, config)
 
         self.save()
 
     @staticmethod
-    def delete(config):
+    def delete(config: str):
+        """Deletes a configuration using its key.
+
+        Args:
+            config (str): configuration key.
+
+        """
+
         self = ConfigManager.__new__(ConfigManager)
         self.__init__()
 
         value_to_delete = self[config]
 
         del self[config]
-        self.logger.debug(f'Deleted config value {value_to_delete!r} with key {config!r}')
+        self.logger.debug('Deleted config value %r with key %r', value_to_delete, config)
         self.save(force=True)
 
     @staticmethod
-    def list():
+    def list() -> tuple:
+        """Returns a list of the keys of the configurations.
+
+        Returns:
+            Tuple[str]: tuple with all the keys.
+        """
         self = ConfigManager.__new__(ConfigManager)
         self.__init__()
 
         result = tuple(self.config.keys())
 
-        self.logger.debug(f'Returning config keys - {result}', enable=False)
+        self.logger.debug('Returning config keys - %s', result)
 
         return result
