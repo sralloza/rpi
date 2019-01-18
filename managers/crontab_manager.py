e69121edd809b7c9c454bed309202bbb717a79f0
sralloza <sralloza@gmail.com>
2019-01-18 10:29:58 +0100

    pylinted crontab manager

--- managers/crontab_manager.py
+++ managers/crontab_manager.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+"""Manager for linux crontab."""
+
+import logging
 import os
 import re
 from typing import Union, List
@@ -8,15 +12,15 @@ from crontab import CronTab, CronItem
 from rpi import operating_system
 from rpi.exceptions import JobNotFoundError, ExistingJobError, InvalidArgumentError
 from rpi.managers.services_manager import ServicesManager
-from rpi.rpi_logging import Logging
+from rpi.managers.users_manager import User
 
 
-class CrontabManager(object):
+class CrontabManager:
     """Crontab interface"""
     BASE = '/usr/local/bin/python3 '
 
     def __init__(self):
-        self.logger = Logging.get(__file__, __name__)
+        self.logger = logging.getLogger(__name__)
         if operating_system() == 'W':
             self.cron = CronTab(tabfile='D:/PYTHON/raspberry_pi/rpi/crontab.txt')
         else:
@@ -26,11 +30,20 @@ class CrontabManager(object):
         return iter(self.cron)
 
     @staticmethod
-    def new(command, user, hour, minutes):
+    def new(command: str, user: Union[User, str], hour: int, minutes: int):
+        """Creates a new task for crontab.
+
+        Args:
+            command (str): command to execute.
+            user (Union[User, str]): user who wants to execute the command.
+            hour (int): hour of the time when the command will be executed.
+            minutes (int): minutes of the time when the command will be executed.
+
+        """
         self = CrontabManager.__new__(CrontabManager)
         self.__init__()
 
-        self.logger.debug(f'Trying to add a CronItem ({command!r}, {user!r}, {hour!r}, {minutes!r})')
+        self.logger.debug('Trying to add a CronItem (%r, %r, %r, %r)', command, user, hour, minutes)
         username = CrontabManager.user_to_username(user)
 
         new_job = self.cron.new(command, comment=username)
@@ -44,19 +57,28 @@ class CrontabManager(object):
                 counter += 1
 
         if counter > 1:
-            self.logger.critical(f'Job already exists: {new_job!r}')
+            self.logger.critical('Job already exists: %r', new_job)
             raise ExistingJobError(f'Job already exists: {new_job!r}')
 
         self.cron.write()
         self.logger.debug('Job created and saved successfully')
 
     @staticmethod
-    def delete_by_anything(anything):
-        # TODO: UNDERSTAND WHAT THE FUCK DOES THIS DO
+    def delete_by_anything(anything: Union[CronItem, int, str]):
+        """Deletes a job, regardless the type of anything (almost).
+
+        Args:
+            anything (Union[CronItem, int, str]): Can be a CronItem, or a hascode.
+
+        Raises:
+            InvalidArgumentError: if anything is not a CronItem, int or str.
+            JobNotFoundError: if there is no job with that hashcode.
+
+        """
         self = CrontabManager.__new__(CrontabManager)
         self.__init__()
 
-        self.logger.debug(f'Trying to delete by anything - {anything!r}')
+        self.logger.debug('Trying to delete by anything - %r', anything)
 
         if isinstance(anything, CronItem):
             hashcode = CrontabManager.job_to_hash(anything)
@@ -65,17 +87,26 @@ class CrontabManager(object):
         elif isinstance(anything, str):
             hashcode = int(anything)
         else:
-            self.logger.critical(f'Tipo incorrecto ({type(anything).__name__!r})')
-            raise InvalidArgumentError(f'Tipo incorrecto ({type(anything).__name__!r})')
+            self.logger.critical('Incorrect type (%r)', type(anything).__name__)
+            raise InvalidArgumentError(f'Incorrect type ({type(anything).__name__!r})')
 
         return self.delete_by_hash(hashcode)
 
     @staticmethod
-    def delete_by_hash(hashcode):
+    def delete_by_hash(hashcode: int):
+        """Deletes a job given its hash.
+
+        Args:
+            hashcode (int): hashcode of the job to delete.
+
+        Raises:
+            JobNotFoundError: if there is no job with that hashcode.
+
+        """
         self = CrontabManager.__new__(CrontabManager)
         self.__init__()
 
-        self.logger.debug(f'Trying to delete job by hash - {hashcode}')
+        self.logger.debug('Trying to delete job by hash - %s', hashcode)
 
         select = None
         jobs = list(self.cron)
@@ -86,48 +117,88 @@ class CrontabManager(object):
                 self.cron.remove(job)
 
         if select is None:
-            self.logger.critical(f'Can not find job with hash={hashcode!r}')
+            self.logger.critical('Can not find job with hash=%r', hashcode)
             raise JobNotFoundError(f'Can not find job with hash={hashcode!r}')
 
         self.cron.write()
-        self.logger.debug(f'Job successfully deleted - {select!r}')
+        self.logger.debug('Job successfully deleted - %r', select)
 
     @staticmethod
-    def list_by_user(user):
+    def list_by_user(user) -> tuple:
+        """Returns a list of the cronitems that belong to a certain user.
+
+        Args:
+            user (Union[User, str]): user to get the list of cronitems from.
+
+        Returns:
+            tuple: list of cronitems of the user.
+
+        """
         self = CrontabManager.__new__(CrontabManager)
         self.__init__()
 
         username = CrontabManager.user_to_username(user)
-        result = list(self.cron.find_comment(username))
+        result = tuple(self.cron.find_comment(username))
 
-        logger = Logging.get(__file__, __name__)
-        logger.debug(f'Returning list of cronitems by user {username!r}', enable=False)
+        logger = logging.getLogger(__name__)
+        logger.debug('Returning list of cronitems by user %r', username)
 
         return result
 
     @staticmethod
-    def job_to_hash(job: CronItem):
-        logger = Logging.get(__file__, __name__)
+    def job_to_hash(job: CronItem) -> int:
+        """Returns a hash from a job.
+
+        Args:
+            job (CronItem): job to get the hash from.
+
+        Returns:
+            int: hash of the cronitem.
+
+        """
+        logger = logging.getLogger(__name__)
         result = hash(str(vars(job)))
 
-        logger.debug(f'Returning hash {result!r} from job {job!r}', enable=False)
+        logger.debug('Returning hash %r from job %r', result, job)
         return result
 
     @staticmethod
-    def user_to_username(user):
+    def user_to_username(user: Union[User, str]) -> str:
+        """Returns the username from a user.
+
+        Args:
+            user (Union[User, str]): user to get the username from.
+
+        Returns:
+            str: username from the user.
+
+        """
         try:
             username = user.username
         except AttributeError:
             username = user
-        logger = Logging.get(__file__, __name__)
-        logger.debug(f'Returning username {username!r} from user {user!s}', enable=False)
+        logger = logging.getLogger(__name__)
+        logger.debug('Returning username %r from user %s', username, user)
         return username
 
     @staticmethod
-    def job_to_str(job: CronItem, admin=False):
-        command_pattern = re.compile(r'(?P<PYTHON>[\w/.]+)\s(?P<SERVICE_PATH>[\w/.]+)\s(?P<EXTRA_ARGS>[\w\-\s]+)')
+    def job_to_str(job: CronItem, admin: bool = False) -> tuple:
+        """Parses a job into and returns the service, options and username of it.
 
-        service = ServicesManager.get(command_pattern.search(job.command).groupdict()['SERVICE_PATH'])
+        Args:
+            job (CronItem): Job to parse.
+            admin (bool): if is True, it will return the username too. If not, it will only return
+                the service and options.
+
+        Returns:
+            tuple: information compress in a tuple.
+
+        """
+        command_pattern = re.compile(
+            r'(?P<PYTHON>[\w/.]+)\s(?P<SERVICE_PATH>[\w/.]+)\s(?P<EXTRA_ARGS>[\w\-\s]+)')
+
+        service = ServicesManager.get(
+            command_pattern.search(job.command).groupdict()['SERVICE_PATH'])
 
         options: Union[List[str], str] = os.path.basename(job.command).split(' ')[1:]
 
@@ -137,7 +208,7 @@ class CrontabManager(object):
         except ValueError:
             pass
 
-        for i in range(len(options)):
+        for i, _ in enumerate(options):
             options[i] = options[i].lstrip(service.command.preoption)
 
         i = 0
