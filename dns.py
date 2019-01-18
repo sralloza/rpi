cbc4257d5808350fccca49f5d20072b6ecacd76d
sralloza <sralloza@gmail.com>
2019-01-19 00:22:34 +0100

    Pylinted dns

--- dns.py
+++ dns.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 
+"""Domain Name System of the Raspberry Pi"""
+
 import platform
 import sqlite3
 import warnings
 from difflib import SequenceMatcher
 
-from .exceptions import TooLowAccuracy, UnknownError, DnsError
+from rpi.exceptions import TooLowAccuracy, UnknownError, DnsError
 
 
-# ADVERTENCIA: ESTE ARCHIVO NO USA LOG DEBIDO A ERRORES DE IMPORTACIÓN.
+# WARNING: THIS FILE DOES NOT USE LOG DUE TO IMPORT ERRORS.
 
-class RpiDns(object):
-    """Clase para obtener las direcciones de archivos en la Raspberry Pi."""
+class RpiDns:
+    """Dns for the entire system."""
     if platform.system() == 'Linux':
-        RUTA = '/home/pi/.database/rpi_dns.sqlite'
+        PATH = '/home/pi/.database/rpi_dns.sqlite'
     else:
-        RUTA = 'D:/.database/rpi_dns.sqlite'
+        PATH = 'D:/.database/rpi_dns.sqlite'
 
     def __init__(self):
-        self.con = sqlite3.connect(self.RUTA)
+        self.con = sqlite3.connect(self.PATH)
         self.cur = self.con.cursor()
         self.cur.execute("""CREATE TABLE IF NOT EXISTS "dns" (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,12 +29,12 @@ class RpiDns(object):
         self._check()
 
     def _check(self):
-        """Comprueba que para cada alias exista una versión en windows y otra en linux."""
+        """For every alias checks that exists a windows version and a linux version."""
 
         self.cur.execute("SELECT alias FROM dns")
-        datos = tuple((x[0] for x in self.cur.fetchall()))
-        windows = tuple((x for x in datos if 'windows' in x))
-        linux = tuple((x for x in datos if 'linux' in x))
+        data = tuple((x[0] for x in self.cur.fetchall()))
+        windows = tuple((x for x in data if 'windows' in x))
+        linux = tuple((x for x in data if 'linux' in x))
 
         windows = set((x.replace('windows.', '') for x in windows))
         linux = set((x.replace('linux.', '') for x in linux))
@@ -45,11 +47,11 @@ class RpiDns(object):
         check = windows == linux
 
         if check is False:
-            raise DnsError('Hay diferencia de plataformas: ' + repr(difs))
+            raise DnsError(f'There is a platform difference: {difs!r}')
 
     @staticmethod
     def alias():
-        """Devuelve los alias de las direcciones almacenadas."""
+        """Returns the alias of the addresses stored"""
 
         self = object.__new__(RpiDns)
         self.__init__()
@@ -60,8 +62,8 @@ class RpiDns(object):
         return data
 
     @staticmethod
-    def nuevo(alias, windows, linux):
-        """Inserta un nuevo alias en el registro."""
+    def new(alias, windows, linux):
+        """Creates a new alias."""
 
         self = object.__new__(RpiDns)
         self.__init__()
@@ -74,12 +76,12 @@ class RpiDns(object):
         self.con.close()
 
     @staticmethod
-    def _similar(a, b):
-        """Devuelve la similitud entre a y b en el intervalo [0,1]."""
-        return SequenceMatcher(None, a, b).ratio()
+    def _similar(this, other):
+        """Returns the similarity between this and other in the interval [0,1]."""
+        return SequenceMatcher(None, this, other).ratio()
 
     def _get(self, alias):
-        """Devuelve la dirección cuyo alias más se parecza a key."""
+        """Returns the address whose alias is more close to alias."""
 
         ratios = {x: self._similar(alias.lower(), x.lower()) for x in self.alias()}
         max_ratio = max(ratios.values())
@@ -97,7 +99,7 @@ class RpiDns(object):
 
     @staticmethod
     def get(alias):
-        """Devuelve la dirección real a partir de un alias."""
+        """Returns the address given its alias."""
 
         self = object.__new__(RpiDns)
         self.__init__()
@@ -105,7 +107,7 @@ class RpiDns(object):
         splitted = alias.split('.')
 
         if len(splitted) < 2:
-            raise DnsError('Se deben aportar 2 o 3 etiquetas')
+            raise DnsError('Must give 2 or 3 tags.')
         if len(splitted) == 2:
             alias = platform.system().lower() + '.' + '.'.join(splitted)
 
@@ -113,8 +115,8 @@ class RpiDns(object):
 
         if alias not in self.alias():
             return self._get(alias)
-        else:
-            self.cur.execute("SELECT address FROM dns WHERE alias=?", (alias,))
-            data = self.cur.fetchone()
-            self.con.close()
-            return data[0]
+
+        self.cur.execute("SELECT address FROM dns WHERE alias=?", (alias,))
+        data = self.cur.fetchone()
+        self.con.close()
+        return data[0]
